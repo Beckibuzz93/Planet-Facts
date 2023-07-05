3dfef546f0a6ab53bb02b9be5cfcaeea5de9b901
Beckibuzz93 <beckilewis1@hotmail.com>
2023-07-06 00:22:48 +0100

    add new folders, change font sizes to rem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
+//const styles = ({ isActive }) => ({ color: isActive ? '#000000' : '#929292' });
 
 function App() {
 
   return (
     <>
     <h1> App </h1>
+    <h2> App </h2>
+    <h3> App </h3>
+    <h4> App </h4>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum nesciunt mollitia officia commodi odio rem dolore fuga ratione aliquam doloribus. Deleniti veritatis vitae atque suscipit itaque, corrupti fugit? Quasi, molestiae!</p>
     </>
   )
 }
