10fe72348fbf527303fa86793d1e9f6c8725d9bd
Beckibuzz93 <beckilewis1@hotmail.com>
2023-07-13 01:08:55 +0100

    add links to the landing page buttons

--- src/pages/Landing Page/index.jsx
+++ src/pages/Landing Page/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import {MainTitle} from '../../components'
 import {MainStatement} from '../../components'
@@ -12,14 +13,14 @@ const LandingPage = () => {
         <MainTitle mainTitle='The Planets' />
         <MainStatement mainStatement='A place to learn about our planets in the solar system. Choose which planet to learn about first below.' />
         <div className='landing-btn-container'>
-          <Buttons.MercuryBtn num='01' buttonName='Mercury'/>
-          <Buttons.VenusBtn num='02' buttonName='Venus'/>
-          <Buttons.EarthBtn num='03' buttonName='Earth'/>
-          <Buttons.MarsBtn num='04' buttonName='Mars'/>
-          <Buttons.JupiterBtn num='05' buttonName='Jupiter'/>
-          <Buttons.SaturnBtn num='06' buttonName='Saturn'/>
-          <Buttons.UranusBtn num='07' buttonName='Uranus'/>
-          <Buttons.NeptuneBtn num='08' buttonName='Neptune'/>
+        <Link to="/Mercury"><Buttons.MercuryBtn num='01' buttonName='Mercury'/></Link>
+        <Link to="/Venus"><Buttons.VenusBtn num='02' buttonName='Venus'/></Link>
+        <Link to="/Earth"><Buttons.EarthBtn num='03' buttonName='Earth'/></Link>
+        <Link to="/Mars"><Buttons.MarsBtn num='04' buttonName='Mars'/></Link>
+        <Link to="/Jupiter"><Buttons.JupiterBtn num='05' buttonName='Jupiter'/></Link>
+        <Link to="/Saturn"><Buttons.SaturnBtn num='06' buttonName='Saturn'/></Link>
+        <Link to="/Uranus"><Buttons.UranusBtn num='07' buttonName='Uranus'/></Link>
+        <Link to="/Neptune"><Buttons.NeptuneBtn num='08' buttonName='Neptune'/></Link>
         </div>
         
     </>
